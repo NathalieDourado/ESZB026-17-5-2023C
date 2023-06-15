e0212aa39327aef01446198a274b6b5e8c7463c9
NathalieDourado <114197961+NathalieDourado@users.noreply.github.com>
2023-06-15 20:22:20 -0300

    Update test2.sh

--- lab02/test2.sh
+++ lab02/test2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /sys/class/gpio  #redireciona para o sysfs que representa o GPIO
+cd /sys/class/gpio
 
 echo 16 >export  #habilita a saida da porta do LED amarelo
 sleep 1
@@ -9,33 +9,37 @@ sleep 1
 echo 21 >export  #habilita a saida da porta do LED verde
 sleep 1
 
-cd gpio20
-echo out > direction
-echo 1 > value
-sleep 2
-echo 0 > value
-cd ..
+x=1
 
-cd gpio21
-echo out > direction
-echo 1 > value
-sleep 1
-echo 0 > value
-cd ..
+while [$x -le 5]
+do
 
-cd gpio16
-echo out > direction
-echo 1 > value
-sleep 1
-echo 0 > value
-cd ..
+ cd gpio20
+ echo out > direction
+ echo 1 > value
+ sleep 2
+ echo 0 > value
+ cd ..
+
+ cd gpio21
+ echo out > direction
+ echo 1 > value
+ sleep 1
+ echo 0 > value
+ cd ..
+
+ cd gpio16
+ echo out > direction
+ echo 1 > value
+ sleep 1
+ echo 0 > value
+ cd ..
+
+ x=$(( $x + 1 ))
+done
 
 echo 16 > unexport
 sleep 1
 echo 20 > unexport
 sleep 1
 echo 21 > unexport
-
-
-
-
