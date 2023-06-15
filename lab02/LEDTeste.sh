81e9ee4a0acad00bba99951423f9af574b940f78
NathalieDourado <114197961+NathalieDourado@users.noreply.github.com>
2023-06-15 20:35:02 -0300

    Delete LEDTeste.sh

--- lab02/LEDTeste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LED_AM=16  
-echo $LED_AM >> "/sys/class/gpio/export"
-echo $1 >> "/sys/class/gpio/gpio$LED_AM/value"
-sleep 3
-echo $0 >> "/sys/class/gpio/gpio$LED_AM/value"
-echo $LED_AM >> "/sys/class/gpio/unexport"
-
